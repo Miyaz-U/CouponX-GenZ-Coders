8d5ea7bc058b501bd6adaf1cb9114433e7f4e443
miyaz <miyazumare@gmail.com>
2026-08-17 22:05:12 +0530

    Minor Changes

--- Backend/server.js
+++ Backend/server.js
@@ -8,19 +8,19 @@ dotenv.config()
 
 // App setup
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 // Middleware
-app.use(express.json());
+app.use(express.json())
 
 // Static folder
-app.use(express.static(path.join(__dirname, '../Frontend')));
+app.use(express.static(path.join(__dirname, '../Frontend')))
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err))
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
-});
+  console.log(`http://localhost:${port}`)
+})
