247db4c2ee795b2d5d3b6591ed67782e8c7c4350
miyaz <miyazumare@gmail.com>
2026-08-17 20:54:51 +0530

    Initial Commit

---
+++ Backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const path = require("path")
+const dotenv = require("dotenv")
+
+// Initializing environment file
+dotenv.config()
+
+// App setup
+const app = express()
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Static folder
+app.use(express.static(path.join(__dirname, '../Frontend')));
+
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
